55ada002362122404fc24534586dc15ff5dd35b1
Sulogna Roy <74129295+sulogna2001@users.noreply.github.com>
2021-08-31 13:18:13 +0530

    Add files via upload

---
+++ src/components/TransactionList.js
@@ -0,0 +1,22 @@
+import React ,{useContext}from 'react'
+import { GlobalContext } from '../context/GlobalState'
+import { Transaction } from './Transaction';
+
+export const TransactionList = () => {
+    const {transactions}=useContext(GlobalContext);
+    
+    return (
+        <div>
+            <div className="history">
+                <h3>History</h3>
+                <ul className="list">
+                    {transactions.map(transaction =>( 
+                    <Transaction 
+                        key={transaction.id} 
+                        transaction={transaction}
+                    />))}
+                </ul>
+            </div>
+        </div>
+    )
+}
